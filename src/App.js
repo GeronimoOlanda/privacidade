b2e01fb55eda26977718b89a5ebe89747653084d
GeronimoOlanda <geronimo.olanda@outlook.com>
2023-12-05 20:35:28 -0300

    ajuste privacidade

--- src/App.js
+++ src/App.js
@@ -2,22 +2,66 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const politicaDePrivacidadeStyle = {
+    textAlign: 'center',
+    // Adicione outros estilos conforme necessário
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={politicaDePrivacidadeStyle}>
+      <h1>Getha - Anotações</h1>
+      <p>
+        <strong>Política de Privacidade para o Seu Aplicativo</strong>
+        <br />
+        <br />
+        <strong>Última atualização:</strong> [05/12/2023 às 14:30]
+        <br />
+        <br />
+        <strong>1. Informações que Coletamos:</strong>
+        <br />
+     Não coletamos nenhuma informação do usuario, todos os dados são armazenados no seu proprio dispositivo.
+        <br />
+        <br />
+        <strong>2. Uso das Informações:</strong>
+        <br />
+       Não usamos nenhuma informação
+        <br />
+        <br />
+        <strong>3. Compartilhamento de Informações:</strong>
+        <br />
+        Não compartilhamos informação, a sua segurança é o que importa. 
+         <br />
+        <br />
+        <strong>4. Segurança:</strong>
+        <br />
+        Empregamos medidas de segurança para proteger as informações contra acesso não autorizado, alteração, divulgação ou destruição não autorizada.
+        <br />
+        <br />
+        <strong>5. Cookies:</strong>
+        <br />
+            No momento, não utilizamos Cookies
+        <br />
+        <br />
+        <strong>6. Alterações na Política de Privacidade:</strong>
+        <br />
+        Esta política de privacidade pode ser atualizada periodicamente para refletir alterações nas práticas de privacidade. Recomendamos que os usuários revisem regularmente esta política para se manterem informados.
+        <br />
+        <br />
+        <strong>7. Direitos do Usuário:</strong>
+        <br />
+        Os usuários têm o direito de acessar, corrigir, excluir ou transferir suas informações pessoais. Para exercer esses direitos ou fazer perguntas sobre esta política, entre em contato conosco através do email getha.olanda@gmail.com.
+        <br />
+        <br />
+        <strong>8. Consentimento:</strong>
+        <br />
+        Ao utilizar o Seu Aplicativo, o usuário concorda com os termos desta política de privacidade.
+        <br />
+        <br />
+        Para mais informações sobre como suas informações são tratadas, entre em contato conosco em getha.olanda@gmail.com.
+        <br />
+        <br />
+        Obrigado por escolher o Getha - Anotações!
+      </p>
     </div>
   );
 }
